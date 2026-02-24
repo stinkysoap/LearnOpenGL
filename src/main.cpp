6c1f262e9b1dd5e9cb0c1220b5a55d615f50d0ae
mudassir <sadiyamuddu2008@gmail.com>
2026-02-24 12:02:18 +0530

    initial-setup-done

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,17 @@
-#include "GLFW/glfw3.h"
+#include <glad/glad.h>
+
+#include <GLFW/glfw3.h>
 #include <iostream>
-int main() { std::cout << "Hello World"; }
+int main() {
+  glfwInit();
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+  GLFWwindow *window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
+  if (window == NULL) {
+    std::cout << "Failed to create GLFW window" << std::endl;
+    glfwTerminate();
+    return -1;
+  }
+  glfwMakeContextCurrent(window);
+}
