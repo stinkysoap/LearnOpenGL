969b624c42d69567f9999d6bbf6a7e7f1565f3f9
Mohammed Mudassir <164621152+stinkysoap@users.noreply.github.com>
2026-02-25 16:20:00 +0530

    ok ok

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,4 @@
-#include<glad/glad.h>
+#include <glad/glad.h>
 #include <iostream>
 
 #include <GLFW/glfw3.h>
@@ -11,7 +11,8 @@ int main() {
   glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
   glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
   glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-  GLFWwindow *window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "LearnOpenGL", NULL, NULL);
+  GLFWwindow *window =
+      glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "LearnOpenGL", NULL, NULL);
   if (window == NULL) {
     std::cout << "Failed to create GLFW window" << std::endl;
     glfwTerminate();
@@ -24,26 +25,25 @@ int main() {
   }
   glViewport(0, 0, 300, 200);
   glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
-  //render Loop
+  // render Loop
   while (!glfwWindowShouldClose(window)) {
-      //input
-      processInput(window);
-      //rendering commands here
-      glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
-      glClear(GL_COLOR_BUFFER_BIT);
+    // input
+    processInput(window);
+    // rendering commands here
+    glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT);
 
-      //check and call events and swap buffers
-      glfwSwapBuffers(window);
-      glfwPollEvents();
+    // check and call events and swap buffers
+    glfwSwapBuffers(window);
+    glfwPollEvents();
   }
   glfwTerminate();
   return 0;
 }
 
-void processInput(GLFWwindow *window)
-{
-    if(glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
-        glfwSetWindowShouldClose(window, true);
+void processInput(GLFWwindow *window) {
+  if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+    glfwSetWindowShouldClose(window, true);
 }
 void framebuffer_size_callback(GLFWwindow *window, int width, int height) {
   glViewport(0, 0, width, height);
