caed085795f4189d33ec319123e0497ed8a1a163
vnr_pc <college@vnr.com>
2026-07-28 06:11:28 -0100

    Window hehe

---
+++ src/main.cpp
@@ -0,0 +1,18 @@
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <iostream>
+
+int main() {
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR,3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR,3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);  
+    GLFWwindow* window = glfwCreateWindow(800,600,"LearnOpenGL",NULL,NULL);
+    if (window ==NULL){
+        std::cout<< "Failed to create window \n" ; 
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+    return 0;
+}
