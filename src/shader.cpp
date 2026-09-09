f7d1d0e3cace15f1af2ba6a8961e6716671441d7
Mohammed Mudassir <sadiyamuddu2008@gmail.com>
2026-09-09 22:16:25 +0530

    thoda kam kiya hai bhai

--- src/shader.cpp
+++ src/shader.cpp
@@ -33,4 +33,48 @@ Shader::Shader(const char *vertexPath, const char *fragmentPath) {
   GLuint vertex, fragment;
   int success;
   char infolog[512];
+
+  // vertex shader
+  vertex = glCreateShader(GL_VERTEX_SHADER);
+  glShaderSource(vertex, 1, &vShaderCode, NULL);
+  glGetShaderiv(vertex, GL_COMPILE_STATUS, &success);
+  if (!success) {
+    glGetShaderInfoLog(vertex, 512, NULL, infolog);
+    std::cout << "ERRROR::SHADER::VERTEX::COMPILATION_FAILED\n"
+              << infolog << std::endl;
+  };
+  fragment = glCreateShader(GL_FRAGMENT_SHADER);
+  glShaderSource(fragment, 1, &vShaderCode, NULL);
+  glGetShaderiv(fragment, GL_COMPILE_STATUS, &success);
+  if (!success) {
+    glGetShaderInfoLog(fragment, 512, NULL, infolog);
+    std::cout << "ERRROR::SHADER::VERTEX::COMPILATION_FAILED\n"
+              << infolog << std::endl;
+  };
+  ID = glCreateProgram();
+  glAttachShader(ID, vertex);
+  glAttachShader(ID, fragment);
+  glLinkProgram(ID);
+  // print linking errors if any
+  glGetProgramiv(ID, GL_LINK_STATUS, &success);
+  if (!success) {
+    glGetProgramInfoLog(ID, 512, NULL, infolog);
+    std::cout << "ERROR::SHADER::PROGRAM::LINKING_FAILED\n"
+              << infolog << std::endl;
+  }
+
+  // delete the shaders as they're linked into our program now and no longer
+  // necessary
+  glDeleteShader(vertex);
+  glDeleteShader(fragment);
+}
+void Shader::use() { glUseProgram(ID); }
+void Shader::setBool(const std::string &name, bool value) const {
+  glUniform1i(glGetUniformLocation(ID, name.c_str()), (int)value);
+}
+void Shader::setInt(const std::string &name, int value) const {
+  glUniform1i(glGetUniformLocation(ID, name.c_str()), value);
+}
+void Shader::setFloat(const std::string &name, float value) const {
+  glUniform1f(glGetUniformLocation(ID, name.c_str()), value);
 }
